0e6d10ce1b6d0f6c1045612e429b2ee0aeb602b4
honzi <czechinthemail@gmail.com>
2016-07-16 22:39:44 -0700

    fixed common settings.js usage

--- js/snakish.js
+++ js/snakish.js
@@ -7,7 +7,7 @@ function eat_purple_creature(){
     ) + 1;
 
     // If there is no space available for more holes, stop.
-    var holes_per_point = settings['holes-per-point'];
+    var holes_per_point = settings_settings['holes-per-point'];
     if(document.getElementById('score').innerHTML >=
       Math.floor(398 / (parseInt(holes_per_point, 10) + 1)) + 1){
         stop();
@@ -37,11 +37,11 @@ function eat_purple_creature(){
 function move_player(){
     // Check if game is still running, based on game mode and if frames/score are over max.
     var frames = parseFloat(document.getElementById('frames').innerHTML);
-    var end_game = settings['game-mode'] === 1
+    var end_game = settings_settings['game-mode'] === 1
       ? frames <= 0
-        && settings['max-frames'] > 0
-      : settings['max-points'] != 0
-        && parseInt(document.getElementById('score').innerHTML, 10) >= settings['max-points'];
+        && settings_settings['max-frames'] > 0
+      : settings_settings['max-points'] != 0
+        && parseInt(document.getElementById('score').innerHTML, 10) >= settings_settings['max-points'];
 
     // If game is not running, stop().
     if(end_game){
@@ -51,8 +51,8 @@ function move_player(){
 
     // Add or subtract 1 from frames depending on game mode.
     document.getElementById('frames').innerHTML = (frames +
-        ((settings['game-mode'] === 1
-      && settings['max-frames'] > 0) ? -1 : 1));
+        ((settings_settings['game-mode'] === 1
+      && settings_settings['max-frames'] > 0) ? -1 : 1));
 
     var check_color = 0;
     var dx = 0;
@@ -82,8 +82,8 @@ function move_player(){
             }
 
         // If player is at the top of the screen and can wrap in the Y-direction.
-        }else if(settings['wrap'] == 2
-          || settings['wrap'] == 3){
+        }else if(settings_settings['wrap'] == 2
+          || settings_settings['wrap'] == 3){
             // Fetch color of space at the bottom of the screen.
             check_color = document.getElementById((player['y'] + 19) * 20 + player['x']).style.backgroundColor;
 
@@ -132,8 +132,8 @@ function move_player(){
             }
 
         // If player is at the right edge of the screen and can wrap in the X-direction.
-        }else if(settings['wrap'] == 1
-          || settings['wrap'] == 2){
+        }else if(settings_settings['wrap'] == 1
+          || settings_settings['wrap'] == 2){
             // Fetch color of space at the left of the screen.
             check_color = document.getElementById(player['y'] * 20 + player['x'] - 19).style.backgroundColor;
 
@@ -182,8 +182,8 @@ function move_player(){
             }
 
         // If player is at the bottom edge of the screen and can wrap in the U-direction.
-        }else if(settings['wrap'] == 2
-          || settings['wrap'] == 3){
+        }else if(settings_settings['wrap'] == 2
+          || settings_settings['wrap'] == 3){
             // Fetch color of space at the top of the screen.
             check_color = document.getElementById((player['y'] - 19) * 20 + player['x']).style.backgroundColor;
 
@@ -231,8 +231,8 @@ function move_player(){
             }
 
         // If player is at the left edge of the screen and can wrap in the X-direction.
-        }else if(settings['wrap'] == 1
-          || settings['wrap'] == 2){
+        }else if(settings_settings['wrap'] == 1
+          || settings_settings['wrap'] == 2){
             // Fetch color of space at the right side of the screen.
             check_color = document.getElementById(player['y'] * 20 + player['x'] + 19).style.backgroundColor;
 
@@ -261,11 +261,11 @@ function move_player(){
     // If a collision with an obstacle or edge was detected.
     if(end_game){
         // If game ends oncollision...
-        if(settings['oncollision'] == 1){
+        if(settings_settings['oncollision'] == 1){
             stop();
 
         // ...else if score decreases.
-        }else if(settings['oncollision'] === 2){
+        }else if(settings_settings['oncollision'] === 2){
             document.getElementById('score').innerHTML =
               parseInt(
                 document.getElementById('score').innerHTML,
@@ -301,8 +301,10 @@ function settings_toggle(state){
 }
 
 function start(){
+    settings_save();
+
     // Set margin-top of game-div based on y-margin.
-    document.getElementById('game-div').style.marginTop = settings['y-margin'] + 'px';
+    document.getElementById('game-div').style.marginTop = settings_settings['y-margin'] + 'px';
 
     // Reset buttons to empty with player and purple creature in initial positions.
     var loop_counter = 399;
@@ -322,9 +324,9 @@ function start(){
     player['y'] = 1;
 
     // Create initial holes, if any.
-    if(settings['holes-at-start'] > 0){
+    if(settings_settings['holes-at-start'] > 0){
         var id = -1;
-        loop_counter = settings['holes-at-start'] - 1;
+        loop_counter = settings_settings['holes-at-start'] - 1;
         do{
             do{
                 id = Math.floor(Math.random() * 400);
@@ -334,9 +336,9 @@ function start(){
     }
 
     // Setup display or not display of max frames or max points.
-    if(settings['game-mode'] === 1){
-        document.getElementById('frames').innerHTML = settings['max-frames'];
-        document.getElementById('frames-max').innerHTML = settings['max-frames'];
+    if(settings_settings['game-mode'] === 1){
+        document.getElementById('frames').innerHTML = settings_settings['max-frames'];
+        document.getElementById('frames-max').innerHTML = settings_settings['max-frames'];
         document.getElementById('score-max').innerHTML = '';
         document.getElementById('frames-max-span').style.display =
           document.getElementById('max-frames').value > 0
@@ -346,19 +348,16 @@ function start(){
     }else{
         document.getElementById('frames').innerHTML = 0;
         document.getElementById('frames-max-span').style.display = 'none';
-        document.getElementById('score-max').innerHTML = settings['max-points'] > 0
-          ? ' / <b>' + settings['max-points'] + '</b>'
+        document.getElementById('score-max').innerHTML = settings_settings['max-points'] > 0
+          ? ' / <b>' + settings_settings['max-points'] + '</b>'
           : '';
     }
-
-    // Save settings.
-    save();
 
     // Validate milliseconds per player movement and create interval.
     interval = window.setInterval(
       move_player,
-      settings['ms-per-move'] > 0
-        ? settings['ms-per-move']
+      settings_settings['ms-per-move'] > 0
+        ? settings_settings['ms-per-move']
         : 125
     );
 }
@@ -366,7 +365,7 @@ function start(){
 function stop(){
     window.clearInterval(interval);
     document.getElementById('start-button').value =
-      'Start [' + settings['start-key'] + ']';
+      'Start [' + settings_settings['start-key'] + ']';
     document.getElementById('start-button').onclick = start;
 }
 
@@ -403,37 +402,37 @@ window.onkeydown = function(e){
     key = String.fromCharCode(key);
 
     // If player wants to move up (if player is moving down then check if 180 degree turns are legal).
-    if(key === settings['movement-keys'][0]
-      && (player['movement_direction'] !== 2 || settings['turn-angle'] == 1)){
+    if(key === settings_settings['movement-keys'][0]
+      && (player['movement_direction'] !== 2 || settings_settings['turn-angle'] == 1)){
         // Player move direction = up.
         player['movement_direction'] = 0;
 
     // If player wants to move right (if player is moving left then check if 180 degree turns are legal).
-    }else if(key === settings['movement-keys'][1]
-      && (player['movement_direction'] !== 1 || settings['turn-angle'] == 1)){
+    }else if(key === settings_settings['movement-keys'][1]
+      && (player['movement_direction'] !== 1 || settings_settings['turn-angle'] == 1)){
         // Player move direction = left.
         player['movement_direction'] = 3;
 
     // If player wants to move down (if player is moving up then check if 180 degree turns are legal).
-    }else if(key === settings['movement-keys'][2]
-      && (player['movement_direction'] !== 0 || settings['turn-angle'] == 1)){
+    }else if(key === settings_settings['movement-keys'][2]
+      && (player['movement_direction'] !== 0 || settings_settings['turn-angle'] == 1)){
         // Player move direction = down.
         player['movement_direction'] = 2;
 
     // If player wants to move left (if player is moving right then check if 180 degree turns are legal).
-    }else if(key === settings['movement-keys'][3]
-      && (player['movement_direction'] !== 3 || settings['turn-angle'] == 1)){
+    }else if(key === settings_settings['movement-keys'][3]
+      && (player['movement_direction'] !== 3 || settings_settings['turn-angle'] == 1)){
         // Player move direction = right.
         player['movement_direction'] = 1;
 
-    }else if(key === settings['start-key']){
+    }else if(key === settings_settings['start-key']){
         stop();
         start();
     }
 };
 
 window.onload = function(){
-    init_settings(
+    settings_init(
       'Snakish.htm-',
       {
         'audio-volume': 1,
@@ -466,11 +465,11 @@ window.onload = function(){
         + '<tr><td><select id=turn-angle><option value=0>90</option><option value=1>&lt;=180</option></select><td>° Turn Angle'
         + '<tr><td><select id=wrap><option value=0>No</option><option value=1>X</option><option value=2>X&amp;Y</option><option value=3>Y</option></select><td>Wrap'
         + '<tr><td><input id=y-margin><td>Y Margin'
-        + '<tr><td colspan=2><input id=reset-button onclick=reset() type=button value=Reset>';
-    update_settings();
+        + '<tr><td colspan=2><input id=reset-button onclick=settings_reset() type=button value=Reset>';
+    settings_update();
 
     // Set margin-top of game-div based on y-margin.
-    document.getElementById('game-div').style.marginTop = settings['y-margin'] + 'px';
+    document.getElementById('game-div').style.marginTop = settings_settings['y-margin'] + 'px';
 
     // Create buttons for game-div.
     var output = '';
