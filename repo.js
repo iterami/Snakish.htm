bc41f7baeca600310e41490242bb2ed1dc891044
Jan Hořava <czechinthemail@gmail.com>
2025-06-19 14:02:31 -0700

    changed storage-controls to storage_controls

--- repo.js
+++ repo.js
@@ -304,7 +304,7 @@ function repo_init(){
         'width': 25,
         'wrap': 0,
       },
-      'storage-controls': true,
+      'storage_controls': true,
       'storage-menu': '<table><tr><td><input class=mini id=height min=1 step=any type=number><td>Button Height'
         + '<tr><td><input class=mini id=width min=1 step=any type=number><td>Button Width'
         + '<tr><td><select id=collision><option value=1>End Game<option value=0>Nothing<option value=2>Score-1</select><td>Collision'
